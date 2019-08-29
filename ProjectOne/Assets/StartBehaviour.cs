b17fef44b53956b4fecffdf2d2dff8ef2ea27555
Abigail Hullinger <10804479@my.uvu.edu>
2019-08-29 17:12:37 -0600

    Print "message" scripts

---
+++ ProjectOne/Assets/StartBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+     print(message: "Hello There");   
+    }
+   
+}
