5591af8210a8719ce9c659fcd9577271f868497c
Abigail Hullinger <10804479@my.uvu.edu>
2019-08-30 13:49:05 -0600

    Added cube to do a practice behaviour script (Unity tutorial video)
    (When you click R, it changes the cube's color) from Unity's tutorial video "C# Scripts as Behaviour Components"

---
+++ ProjectOne/Assets/PracticeBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeBehaviourScript : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _renderer.material.color = new Color(1f, 0.51f, 0.43f);
+        }
+    }
+}
