f7dc3a611e1448f8d3a8566d00b1c2a120128970
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-03 15:33:27 -0600

    Variables, collider, rigidbody, particals for cube and sphere
    deleted Unity tutorial script and square sprite

---
+++ ProjectOne/Assets/Variables.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Variables : MonoBehaviour
+{
+    public float floatValue = 5.5f;
+    public int intValue = 20;
+    public string stringValue = "Bob";
+    public int firePower;
+    public UnityEvent Event;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Event.Invoke();
+            
+    }
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
