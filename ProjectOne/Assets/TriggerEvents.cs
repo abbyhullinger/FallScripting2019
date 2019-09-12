22e5d17f39d00e745b4425b880f4ccee653e4081
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-12 14:25:10 -0600

    Changed back to ontriggerevent
    just for now, it looks like we are going to go over onmousedown in class today

--- ProjectOne/Assets/TriggerEvents.cs
+++ ProjectOne/Assets/TriggerEvents.cs
@@ -4,16 +4,16 @@ using UnityEngine.Events;
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEvent;
-    public UnityEvent mouseDown;
+  //  public UnityEvent mouseDown;
     
-  //  private void OnTriggerEnter(Collider other)
- //   {
-  //      triggerEvent.Invoke();
+   private void OnTriggerEnter(Collider other)
+   {
+       triggerEvent.Invoke();
        
-  //  }
+   }
 
-    private void OnMouseDown(Collider other)
-    {
-        triggerEvent.Invoke();
-    }
+   // private void OnMouseDown(Collider other)
+  //  {
+  //      triggerEvent.Invoke();
+  //  }
 }
