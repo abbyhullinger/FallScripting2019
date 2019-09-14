9e9f8f7d4a67ee1e9c7dfa5484b4ab8d4e068a61
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-13 22:15:15 -0600

    Added Birdevents with onmousedrag and particles

---
+++ ProjectOne/Assets/BirdEvents.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
+
+public class BirdEvents : MonoBehaviour
+{
+  public UnityEvent birdEvent;
+
+  private void OnMouseDrag()
+  {
+    birdEvent.Invoke();
+  }
+}
