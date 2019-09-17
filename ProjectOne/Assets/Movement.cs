26cdd6934fadbca092a900e99b759e5d06772e05
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-17 15:58:22 -0600

    CharacterController, Movement script
    Made  a character move horizontal and jump

---
+++ ProjectOne/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float gravity = 3f;
+    public float jumpSpeed = 10f;
+    
+    private Vector3 position;
+    public CharacterController controller;
+    
+    
+    void Update()
+    {
+        
+        position.x = speed * Input.GetAxis("Horizontal");
+// ! MEANS FALSE, OR IS NOT GROUNDED
+        if (!controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+       
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+        controller.Move(position*Time.deltaTime);
+    }
+}
