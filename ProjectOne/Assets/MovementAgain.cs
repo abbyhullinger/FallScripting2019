6002c4543b3e52e6e5fae5f40359f1d4633f848d
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-19 15:57:40 -0600

    Made DoubleJumper in MovementAgain script!!! Deleted the doubleJumpMove
    this was a practice on making a movement script like we did last time in class

---
+++ ProjectOne/Assets/MovementAgain.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA;
+
+//this is the rewritten version of the Movement script for practice.
+//In this code, it will find the character controller on start
+[RequireComponent(typeof(CharacterController))]
+public class MovementAgain : MonoBehaviour
+{
+    private Vector3 position;
+    private CharacterController controller;
+    private int jumpCount;
+    
+    public float moveSpeed = 10f, gravity = 9.81f, jumpSpeed = 30f;
+    public int jumpCountMax = 2; 
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        position.x = moveSpeed*Input.GetAxis("Horizontal");
+        position.y -= gravity;
+
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump")&& jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+        controller.Move(position*Time.deltaTime);
+    }
+}
