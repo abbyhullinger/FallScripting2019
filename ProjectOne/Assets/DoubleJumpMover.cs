995ab4188e8e2dddfc453fa2656eef42c5bf7d8d
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-18 21:49:08 -0600

    Tried to do DoubleJump Script but both items are connected
    This could be useful in some way! But I want to change it

---
+++ ProjectOne/Assets/DoubleJumpMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpMover : MonoBehaviour
+{
+    public float speed = 2f;
+    public float gravity = 3f;
+    public float jumpSpeed = 10f;
+    private int jumpCount = 2;
+
+    private Vector3 position;
+    public CharacterController controller; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = speed * Input.GetAxis("Horizontal");
+
+        if (controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+        else
+        {
+            jumpCount = 1;
+        }
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            position.y = jumpSpeed;
+            jumpCount--;
+        }
+        controller.Move(position * Time.deltaTime);
+    }
+}
