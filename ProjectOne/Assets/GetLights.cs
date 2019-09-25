061cea293f7095966b6a298bf58f5b7a115f9e35
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-25 12:45:44 -0600

    GetLights if and else statement script

---
+++ ProjectOne/Assets/GetLights.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetLights : MonoBehaviour
+{
+    public bool lightIsOn, lightIsOff; 
+   
+    void Start()
+    {
+        if (lightIsOff)
+        {
+            print("It's dark in here!");
+        }
+        else if (lightIsOn)
+        {
+           print("It's too bright!"); 
+        }
+        
+    }
+
+}
