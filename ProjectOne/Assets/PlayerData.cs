800f492da5255a905c518d7aace336161f587f05
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-27 21:48:19 -0600

    Start to PlayerData script, based for a fairy character

---
+++ ProjectOne/Assets/PlayerData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public float fairyDust = 1f;
+    public int flyingPower = 5;
+    public float magicPower = 20f;
+    //fairyDust is weapon, flyingPower is stamina, magicPower is HP;
+    //May need to make these clearer but just for fun;
+
+    public void UpdateFairyDust(float amount)
+    {
+        fairyDust += amount;
+    }
+
+    private void UpdateFlyingPower(int amount)
+    {
+        flyingPower += amount;
+    }
+
+    public void UpdateMagicPower(float amount)
+    {
+        magicPower += amount;
+    }
+}
