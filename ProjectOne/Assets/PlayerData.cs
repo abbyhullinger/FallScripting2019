d46e043b1e482715c35d89036cf2bf7fe40dea74
Abigail Hullinger <10804479@my.uvu.edu>
2019-09-28 21:08:36 -0600

    added some more to PlayerData script

--- ProjectOne/Assets/PlayerData.cs
+++ ProjectOne/Assets/PlayerData.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.XR.WSA;
 
 [CreateAssetMenu]
 public class PlayerData : ScriptableObject
@@ -8,9 +10,12 @@ public class PlayerData : ScriptableObject
     public float fairyDust = 1f;
     public int flyingPower = 5;
     public float magicPower = 20f;
+    public int magicDanceMovesPower = 10;
+    
+    
     //fairyDust is weapon, flyingPower is stamina, magicPower is HP;
     //May need to make these clearer but just for fun;
-
+  
     public void UpdateFairyDust(float amount)
     {
         fairyDust += amount;
@@ -18,11 +23,17 @@ public class PlayerData : ScriptableObject
 
     private void UpdateFlyingPower(int amount)
     {
-        flyingPower += amount;
+        flyingPower -= amount;
     }
 
     public void UpdateMagicPower(float amount)
     {
         magicPower += amount;
     }
+
+    public void UpdatemagicDanceMovesPower(int amount)
+    {
+        magicDanceMovesPower += amount;
+    }
+   
 }
