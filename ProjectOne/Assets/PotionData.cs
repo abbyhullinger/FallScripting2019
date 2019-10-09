5c2aa5ed0e069f6a05ac3134c3a8c75ef31936ac
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-08 22:27:11 -0600

    PotionData Script

---
+++ ProjectOne/Assets/PotionData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PotionData : GameArtData
+{
+    public int potionPower = 10;
+    public Animation animation;
+    public ParticleSystem particles; 
+    
+}
+// not sure how to give different 'potions' or items different effects, for now it could be a;
+//full health regenerator or something!; 
