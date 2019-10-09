a718d3001bb8b6474bc1821f97ad5e415a298823
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-09 16:06:15 -0600

    Shielddata script

--- ProjectOne/Assets/PotionData.cs
+++ ProjectOne/Assets/PotionData.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.WSA;
+
 [CreateAssetMenu]
 public class PotionData : GameArtData
 {
     public int potionPower = 10;
     public Animation animation;
-    public ParticleSystem particles; 
-    
+    public ParticleSystem particles;
+    public WorldAnchor position; 
 }
 // not sure how to give different 'potions' or items different effects, for now it could be a;
 //full health regenerator or something!; 
---
+++ ProjectOne/Assets/ShieldData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+[CreateAssetMenu]
+public class ShieldData : GameArtData
+{
+    public int shieldPower = 10;
+    public Behaviour behaviour;
+    public PositionalLocatorState positionlife;
+    public TimeSpan lifeTime; 
+}
