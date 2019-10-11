a05b01f1f2c5f09ffee2921c32b8c7f30b6eb3fb
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-11 12:38:53 -0600

    Edited previous scripts, made enemydata script
    enemydata has position for where the enemy will sit

---
+++ ProjectOne/Assets/DataScripts/EnemyData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class EnemyData : GameArtData
+{
+    public FloatData health;
+    public Vector3 enemyLocation;
+}
--- ProjectOne/Assets/DataScripts/PlayerData.cs
+++ ProjectOne/Assets/DataScripts/PlayerData.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerData : GameArtData
 {
    public FloatData health;
-   public List<WeaponData> weapons; 
+   public List<WeaponData> weapons;
+   public List<PotionData> potion; 
    public ClothesData shirt;
-   public ClothesData pants; 
+   public ClothesData pants;
+   public ClothesData hat; 
 }
