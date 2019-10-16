ecc8e4c611031e3c9953bc1fb25baaed59235460
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-15 21:43:15 -0600

    CinemaController script, installed cinemachine
    has errors here: PlayerData.instanceAction = InstanceHandler; 
         PlayerData.InstancePlayer(); 
    however this is what worked in class. Needs to be fixed somehow

--- ProjectOne/Assets/DataScripts/PlayerData.cs
+++ ProjectOne/Assets/DataScripts/PlayerData.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class PlayerData : GameArtData
 {
+   public UnityAction<GameObject> instanceAction; 
    public FloatData health;
    public List<WeaponData> weapons;
    public List<PotionData> potion; 
    public ClothesData shirt;
    public ClothesData pants;
-   public ClothesData hat; 
+   public ClothesData hat;
+
+   public void InstancePlayer()
+   {
+      var newPlayer = Instantiate(prefab);
+      var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+      playerSprite.sprite = sprite;
+      playerSprite.color = spriteColor;
+      instanceAction(newPlayer); 
+   }
 }
