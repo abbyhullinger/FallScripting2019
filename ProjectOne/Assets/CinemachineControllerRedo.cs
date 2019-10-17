61fd98d292db9c2e0033262ece62a062140e536e
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-16 22:18:08 -0600

    Deleted unused scripts, rewrote CinemachineController script
    it wasn't working and now it is

---
+++ ProjectOne/Assets/CinemachineControllerRedo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CinemachineControllerRedo : MonoBehaviour
+{
+   public PlayerData playerData;
+   private CinemachineVirtualCamera virtualCamera;
+
+   private void Awake()
+   {
+      virtualCamera = GetComponent<CinemachineVirtualCamera>();
+      playerData.instanceAction = InstanceHandler;
+      playerData.InstancePlayer();
+
+   }
+
+   private void InstanceHandler(GameObject obj)
+   {
+      virtualCamera.Follow = obj.transform;
+   }
+}
