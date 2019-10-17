8a0a9795fd1a8d8a0bab2c4a366c6db8dc2e97a6
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-17 17:17:48 -0600

    Made harmful platform, Unity tutorial animator script

---
+++ ProjectOne/Assets/CharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public Animator anim;
+    private int jumpHash = Animator.StringToHash("Jump");
+    private int runStateHash = Animator.StringToHash("Base Layer.Run");
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        anim.SetFloat("Speed", move);
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if (Input.GetKeyDown(KeyCode.Space) && stateInfo.nameHash == runStateHash)
+        {
+            anim.SetTrigger(jumpHash);
+        }
+    }
+}
