14119a0fc854ee748e3f94b6fea12a6b62aacfeb
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-18 19:36:51 -0600

    Tried to make a 'respawner' script
    not sure if it is making the character move back because the number is small...

---
+++ ProjectOne/Assets/Respawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
+
+public class Respawner : MonoBehaviour
+{
+    public FloatData value;
+    public FloatData minValue;  
+    private void Start()
+    {
+        if (value == minValue)
+        {
+            transform.position = UnityEngine.Vector3.back;
+        }
+    }
+}
