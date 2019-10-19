460395ff49db4391a3d3af5cfa1e51fef6d16c3c
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-19 10:19:48 -0600

    Reworked respawner script
    still isn't working but I think I got closer this time

--- ProjectOne/Assets/Respawner.cs
+++ ProjectOne/Assets/Respawner.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.WSA;
 using Vector3 = System.Numerics.Vector3;
 
 public class Respawner : MonoBehaviour
 {
     public FloatData value;
-    public FloatData minValue;  
+    public FloatData minValue;
+    public Vector3 position;
+ 
     private void Start()
     {
         if (value == minValue)
         {
-            transform.position = UnityEngine.Vector3.back;
+            position.X = 0;
+            position.Y = 0; 
+
         }
     }
 }
