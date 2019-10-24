a6f46d2a5050afc24f83f48f2391ed7a3d616800
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-23 21:42:58 -0600

    gameartcollection script
    tried to destroy sprite to make object 'disappear' when collected..

--- ProjectOne/Assets/DataScripts/GameArtCollection.cs
+++ ProjectOne/Assets/DataScripts/GameArtCollection.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 [CreateAssetMenu]
 public class GameArtCollection : ScriptableObject
 {
     public List<GameArtData> collectionList;
+  
 
     public void AddData(GameArtData obj)
     {
@@ -12,12 +14,14 @@ public class GameArtCollection : ScriptableObject
         {
             collectionList.Add(obj);
             obj.collected = true;
+          
         }
-       
+
+        if (!obj.collected)
+        {
+            SpriteRenderer.Destroy(obj);
+        }
+
     }
     
 }
-//this will receive weapon data, so we can go around and collect weapons in the game if we do List<WeaponDta>;
-//List<SriptableObject> means we can get any scriptable objects but this isn't specific;
-//So we did GameArtData; 
-//the adddata function is going to add gameart to the collection list! and anyone can call it cause it's public;
