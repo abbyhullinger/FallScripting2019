ba579878cc287ba350062802e55a37659b71d919
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-25 11:53:58 -0600

    gameartcollection redo with different loop

---
+++ ProjectOne/Assets/DataScripts/GameArtCollectionRedo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class GameArtCollectionRedo : MonoBehaviour
+{
+   public List<GameArtData> listCollection;
+
+   public void AddData(GameArtData obj)
+   {
+      foreach (var gameArt in listCollection)
+      {
+         if (listCollection.Contains(obj))
+         {
+            listCollection.Add(obj);
+         }
+      }
+   }
+
+   public void RemoveItem()
+   {
+      if (!listCollection.Contains(null))
+      {
+         listCollection.RemoveAt(listCollection.Count-1);
+      }
+   }
+}
