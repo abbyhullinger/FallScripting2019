c6d4e9cbf0f8c8561adb057ec5606664d3567fd9
Abigail Hullinger <10804479@my.uvu.edu>
2019-10-30 22:14:42 -0600

    Weapons script with enum
    lifespan (how long it will last with each use)

---
+++ ProjectOne/Assets/WeaponsHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponsHandler : MonoBehaviour
+{
+    public enum LifeSpan
+    {
+        Life,
+        TimeSpan,
+        Death, 
+    }
+
+    public LifeSpan currentLifeSpan;
+
+    public UnityEvent OnLifeEvent, OnTimeSpanEvent, OnDeathEvent; 
+    void Update()
+    {
+        switch (currentLifeSpan)
+        {
+            case LifeSpan.Life:
+                OnLifeEvent.Invoke();
+                break;
+            case LifeSpan.TimeSpan:
+                OnTimeSpanEvent.Invoke();
+                break;
+            case LifeSpan.Death:
+                OnDeathEvent.Invoke();
+                break;
+        }
+    }
+}
