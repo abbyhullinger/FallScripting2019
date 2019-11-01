3bfd24bade3d2ee5fe4af837e694e6a121612ec4
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-01 15:08:10 -0600

    Enemy handler ai script

---
+++ ProjectOne/Assets/AI/EnemyHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyHandler : MonoBehaviour
+{
+    private NavMeshAgent enemy;
+    private Transform destination;
+    private Transform destinationObj;
+    private NavMeshPath enemyPath; 
+    
+    
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = destinationObj;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        }
+
+    public Transform NavMeshPath { get; set; }
+}
