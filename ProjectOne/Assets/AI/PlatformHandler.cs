6bf88d9790f13fa43eb896bf00f0df54eb69df6b
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-03 15:45:19 -0700

    platformai script

---
+++ ProjectOne/Assets/AI/PlatformHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlatformHandler : MonoBehaviour
+{
+    private NavMeshAgent platformAI;
+    private Transform destination;
+    private  Transform platformMovement;
+    
+    
+    void Start()
+    {
+        platformAI = GetComponent<NavMeshAgent>();
+        destination = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = platformMovement; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
