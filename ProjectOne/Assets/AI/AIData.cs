4b0d05847e3fd57b60f6d38e52fe2aef0eb9849b
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-04 16:59:56 -0700

    Scriptable object ai script start

---
+++ ProjectOne/Assets/AI/AIData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Vector3 = System.Numerics.Vector3;
+
+[CreateAssetMenu]
+public class AIData : GameArtData
+{
+    public NavMeshAgent potion;
+    private Transform currentDestination;
+    private GameObject startObj;
+    public Transform potionMovement;
+       
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter()
+    {
+        currentDestination = potionMovement; 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
