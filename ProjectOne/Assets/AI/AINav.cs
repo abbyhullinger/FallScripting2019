bfe22e2f2b0ceabcde61b61103d98af6e56a7870
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-05 22:01:51 -0700

    AINav script
    clickable ai follower

---
+++ ProjectOne/Assets/AI/AINav.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AINav : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Vector3Data location; 
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+     agent.destination = location.value; 
+    }
+}
---
+++ ProjectOne/Assets/AI/MouseEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvents : MonoBehaviour
+{
+   private Camera cam;
+   public Vector3Data data; 
+
+   private void Start()
+   {
+      cam = Camera.main; 
+   }
+
+   private void OnMouseDown()
+   {
+     var ray = cam.ScreenPointToRay(Input.mousePosition);
+      
+      if (Physics.Raycast(ray, out var hit))
+      {
+         data.value = hit.point; 
+      }
+      
+ }
+   
+}
