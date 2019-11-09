8ee6544e92685a39fc5e7afbd65d388dcead9be8
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-09 12:14:16 -0700

    EnemyAINav script

---
+++ ProjectOne/Assets/AI/EnemyAINav.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAINav : MonoBehaviour
+{
+    private NavMeshAgent enemyAgent;
+    private Vector3Data enemyLocation; 
+   void Start()
+   {
+       enemyAgent = GetComponent<NavMeshAgent>();
+   }
+
+   private void OnTriggerEnter()
+   {
+       enemyAgent.destination = enemyLocation.value;
+   }
+    
+}
