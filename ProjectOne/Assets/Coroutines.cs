f51274339d3ac7fb8f8655a62f19645ba3f2a568
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-13 23:07:05 -0700

    coroutine rewritten script

---
+++ ProjectOne/Assets/Coroutines.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float seconds = 10f;
+    private int counter = 10;
+    public bool canRun;
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke();
+
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        
+        endEvent.Invoke();
+    }
+   
+}
