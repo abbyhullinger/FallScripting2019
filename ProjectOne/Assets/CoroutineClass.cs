7c5cf37a6befeab2bd7ccd83cabb6055736f06c9
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-14 22:29:04 -0700

    coroutineclass script

---
+++ ProjectOne/Assets/CoroutineClass.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
+
+public class CoroutineClass : MonoBehaviour
+{
+    public bool canRun = true;
+    public IntData index;
+    public float seconds = 1f;
+    public bool regeneration = true;
+    public UnityEvent triggerEvent;
+    private WaitForSeconds wfsObj; 
+
+    public void Run()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        StartCoroutine(OnRun());
+    }
+    IEnumerator OnRun()
+    {
+        while (index.value > 0)
+        {
+            Debug.Log(index.value);
+            //index--;
+            yield return wfsObj;
+            
+            triggerEvent.Invoke();
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Run");
+        }
+
+        while (regeneration)
+        {
+            
+        }
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
