647b36728e7c87c7f77dba108556bb99451400f2
Abigail Hullinger <10804479@my.uvu.edu>
2019-11-21 18:05:44 -0700

    GameAction scripts

---
+++ ProjectOne/Assets/Animation/GameActionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public UnityEvent handlerEvent; 
+    void Start()
+    {
+        gameActionObj.action = RaiseHandler; 
+    }
+
+    public void RaiseHandler()
+    {
+        handlerEvent.Invoke();
+    }
+ }
+//this can handle any incoming things, the animation statemachine will fire off game action
+//anything that will handle the gameaction is going to do something, this is deligation
